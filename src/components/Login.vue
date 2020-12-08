84c60f11799cdb8329269b87ba261bc6bc1c4ab9
kojitalk <478816129@qq.com>
2020-12-08 17:49:57 +0800

    第一次提交

---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/timg.jpg" alt="" />
+      </div>
+      <!-- 登录表单区 -->
+      <el-form label-width="0px" class="login_form">
+        <el-form-item>
+          <el-input>
+            <i slot="prefix" class="el-input__icon el-icon-user-solid"></i
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input>
+            <i slot="prefix" class="el-input__icon el-icon-s-goods"></i
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btn">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<style scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.btn {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
